47698c837107e0c13ab05a45155cb801d075b23c
Manga_Bl_Manga <gogo05tv@gmail.com>
2025-05-24 18:33:43 +0300

    added the basic input

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,12 @@
 export default function RootLayout({ children }) {
     return (
       <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Karla:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet" />
+      </head>
+
         <body>{children}</body>
       </html>
     )
--- app/page.jsx
+++ app/page.jsx
@@ -3,10 +3,14 @@ import "./global.css";
 import Markdown from "react-markdown";
 import React, { useEffect, useState } from "react";
 import remarkGfm from "remark-gfm";
+import DatePicker from "react-datepicker"
+import "react-datepicker/dist/react-datepicker.css"
 
 export default function Page() {
   const [data, setData] = useState(null);
 
+  const [tasks, setTasks] = useState([])
+  const [visions, setVisions] = useState([])
   function getResponse() {
 
     for (let i = 0; i < vision.length; i++) {
@@ -31,20 +35,45 @@ export default function Page() {
     fetchData();
   } // end of zeft
 
-  function addVision(formData){
+
+  const visionMap = visions.map((value, index) => {
+    return (
+      <h3 key={index} className="vision">{value}</h3>
+    )
+  })
+
+  function addVision(formData) {
     const newVision = formData.get("vision");
+    setVisions((prev) => { return ([...prev, newVision]) })
     console.log("test vision input, newVision: " + newVision);
   }
-  
-  return (
-    <div className="allTheSHIT">
 
-      <div className="vision">
+
+  const tasksMap = tasks.map((value, index) =>{
+    return (
+      <h3 key={index} className="task">{value}</h3>
+    )
+  })
+
+  function addTasks(tasksFormData) {
+    const newTask = tasksFormData.get("task");
+    setTasks((prev) => [...prev, newTask]);
+    console.log("newTask to be added is" + newTask);
+  }
+
+  const [date, setDate] = useState()
+
+  return (
+    <div className="allTheSHIT bg-yellow-300">
+
+      <div className="visions">
         <form action={addVision}>
-          <input type="text" name="vision"></input>
+          <input type="text" name="vision" />
           <button>add vision</button>
         </form>
-      </div> 
+
+        {visionMap}
+      </div>
 
       <div className="schedule">
         <h1>Your Schedule</h1>
@@ -52,16 +81,32 @@ export default function Page() {
 
       <div className="input">
 
-        <form>
-          <input type="text" name="task"></input>
+        <form action={addTasks}>
+          <input type="text" name="task" />
           <button>enter task</button>
         </form>
 
         <form>
-          <input type="text" name="date"></input>
+          <DatePicker
+            selected={date}
+            onChange={(selectedDate) => setDate(selectedDate)}
+            dateFormat="yyyy-MM-dd"
+            placeholderText="Click to select a date"
+          />
           <button>enter date</button>
         </form>
-      </div>
+
+        <div className="tasks">
+          {tasksMap}
+        </div>
+
+        <div className="date">
+          {date
+            ? date.toDateString() // e.g., "Mon Jun 01 2025"
+            : "No date selected"}
+        </div>
+
+      </div> {/*end of input div*/}
     </div>
   );
 }
